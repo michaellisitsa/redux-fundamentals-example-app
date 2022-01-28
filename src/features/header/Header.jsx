b6583a43500933e60efb41a56915a7964a3e11ea
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro-2.local>
2022-01-28 13:49:46 +1100

    Add Header loading state

--- src/features/header/Header.jsx
+++ src/features/header/Header.jsx
@@ -4,28 +4,41 @@ import { saveNewTodo } from '../todos/todosSlice'
 
 function Header() {
   const [text, setText] = React.useState('')
+  const [status, setStatus] = React.useState('idle')
   const dispatch = useDispatch()
 
   function handleChange(e) {
     setText(e.target.value)
   }
 
-  function handleKeyDown(e) {
+  async function handleKeyDown(e) {
     const trimmedText = e.target.value.trim()
     if (e.key === 'Enter' && trimmedText) {
-      dispatch(saveNewTodo(trimmedText))
+      // Set loading state
+      setStatus('loading')
+      await dispatch(saveNewTodo(trimmedText))
       setText('')
+      setStatus('idle')
     }
   }
+
+  let isLoading = status === 'loading'
+  let placeholder = isLoading ? '' : 'What needs to be done?'
+  let loader = isLoading ? <div className="loader" /> : null
+
   return (
-    <input
-      type="text"
-      placeholder="What needs to be done?"
-      autoFocus={true}
-      value={text}
-      onChange={handleChange}
-      onKeyDown={handleKeyDown}
-    />
+    <header className="header">
+      <input
+        type="text"
+        placeholder="What needs to be done?"
+        autoFocus={true}
+        value={text}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        disabled={isLoading}
+      />
+      {loader}
+    </header>
   )
 }
 
